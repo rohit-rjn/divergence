783809cf03b6399334c333350a3e04d7ef777161
Anthony Federico <anfederico@users.noreply.github.com>
2019-06-07 09:49:55 -0400

    Exception handling

--- divergence_detection/run_divergence.py
+++ divergence_detection/run_divergence.py
@@ -3,53 +3,53 @@ from get_data import get_hourly, get_daily, resample_data
 import json
 import datetime 
 import warnings
+warnings.filterwarnings('ignore')
+
 def get_df(token):
-    #duration = duration.split()
     df = get_hourly(token)
     df_daily = get_daily(token)
     df_weekly = resample_data(df_daily, '1','WEEK')
     df_weekly = df_weekly.reset_index(level=None, drop=False, inplace=False, col_level=0)
-
     return df, df_weekly
 
 def get_signals(token_list,duration):
     signals =[]
     hourly_signal = [[] for _ in range(len(duration))]
-    #hourly_signal= [hourly_signal]*len(duration)
+    
     for i in range(len(token_list)):
-        try:                                                                                                                                         
+        print(token_list[i])
+        try:
             df, df_weekly = get_df(token_list[i])
-        except:
-            message = 'DataFrame '+str(token_list[i])+' not found'
-            warnings.warn(message)
-            continue
+            for j in range(len(duration)):
+                try:
+                    curr_duration = duration[j]                
+                    curr_duration = curr_duration.split()
+                    df_new = resample_data(df, curr_duration[0],curr_duration[1])
+                    df_new = df_new.reset_index(level=None, drop=False, inplace=False, col_level=0)
+                    div_df = find_divergence(df_new, df_weekly, curr_duration[0])
+                    if (div_df is not None):
+                        dict_div = {}
+                        dict_div['token']=token_list[i]
+                        dict_div['duration']=duration[j]
+                        dict_div['start']=div_df['start']
+                        dict_div['end']=div_df['end']
+                        dict_div['type']=div_df['type']
+                        dict_div['cosine']=div_df['cosine']
+                        dict_div['market_state']=div_df['market_state']
+                        dict_div['volatility']=div_df['volatility']
+                        hourly_signal[j].append(dict_div)
+                except Exception as e:
+                    print(e)
+                    print('Skipping '+str(token_list[i])+' on '+str(duration[j]))
+        except Exception as e:
+            print(e)
+            print('Skipping '+str(token_list[i]))
 
-        
-        for j in range(len(duration)):
-            curr_duration = duration[j]
-            #print(curr_duration)
-            
-            curr_duration = curr_duration.split()
-            df_new = resample_data(df, curr_duration[0],curr_duration[1])
-            df_new = df_new.reset_index(level=None, drop=False, inplace=False, col_level=0)
-            div_df = find_divergence(df_new, df_weekly, curr_duration[0])
-            if (div_df is not None):
-                dict_div = {}
-                dict_div['token']=token_list[i]
-                dict_div['duration']=duration[j]
-                dict_div['start']=div_df['start']
-                dict_div['end']=div_df['end']
-                dict_div['type']=div_df['type']
-                dict_div['cosine']=div_df['cosine']
-                dict_div['market_state']=div_df['market_state']
-                dict_div['volatility']=div_df['volatility']
-                hourly_signal[j].append(dict_div)
     signals_small = {}
-    #print(hourly_signal[0])
     for i in range(len(duration)):
         signals_small[duration[i]]=hourly_signal[i]
     json_string=json.dumps(signals_small, default=str)
-    
+
     return json_string
 
 if __name__=="__main__":
